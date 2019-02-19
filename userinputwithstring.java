79e66eb3f23421f7379af0ff6401a00083d0581e
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-02-20 01:32:17 +0600

    Update userinputwithstring.java

--- userinputwithstring.java
+++ userinputwithstring.java
@@ -7,12 +7,14 @@ public class userinputwithstring
     Scanner abesh=new Scanner(System.in);
     System.out.println("Enter name to know about details");
     String name=abesh.nextLine();
-    String a="abesh",b="asif";
+    String a="abesh",b="asif"c="ifty";
     if (name.equalsIgnoreCase(a)){
       System.out.println("He is Single");;
     }
     else if (name.equalsIgnoreCase(b)){
       System.out.println("he is taken");}
+    else if(name.equalsIgnoreCase(c)) {
+      System.out.println("fuck boy he is");}
     else {
       System.out.println("fuck all");
     }

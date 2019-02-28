680b04327a70d49ee274f31e502bfb419acacd40
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-01 05:53:54 +0600

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class while_loop {
+  public static void main (String args[]){
+   int i=1;
+   while (i<=10){
+   System.out.println("abesh");
+   i++;
+   }}}
+     
+    
+                                           

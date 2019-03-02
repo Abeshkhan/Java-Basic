ae6d654a32e81cceace2b112e3f1d518439081f7
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-03 02:58:18 +0600

    Add files via upload

---
+++ SumwithLoop2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SumwithLoop2{
+  public static void main(String args[]){
+    Scanner abesh=new Scanner (System.in);
+    int sum=0;
+      int a=abesh.nextInt(); 
+      int b=abesh.nextInt();
+    for (int i=a;i<=b;i++){
+      sum =sum+i;}
+    System.out.println("Sum="+sum);
+  }}
+     

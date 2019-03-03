48e5edeeff95e19ecccb7f5f0e18e7c652c56f08
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-04 02:08:31 +0600

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,14 @@
+public class Array1{
+  public static void main (String args[]){
+    int[]number= new int[3];
+    number[0] =2;
+    number[1] =2;
+    number[2] =2;
+    
+    int sum = number[0] + number [1] + number[2];
+      System.out.println(sum);
+    
+     int len=number.length;
+    System.out.println("Array size="+len);
+   
+  }}

5d95186aabb80cb8a363af243b8c7b449aea805e
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-27 06:16:23 +0600

    Add files via upload

---
+++ ReverseDigits.java
@@ -0,0 +1,15 @@
+public class ReverseDigits{
+  public static void main(String args[]){
+    java.util.Scanner abesh=new java.util.Scanner (System.in){
+      System.out.println("Enter number");
+      int num=abesh.nextInt();
+      int sum=0,r,temp;
+      temp=num;
+      while(num!=0){
+        r=temp%10;
+        sum=sum*10+r;
+        temp=temp/10;
+      }
+    System.out.println(" Reverse of digitis : "+sum);
+    }}}
+        

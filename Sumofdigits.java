1b4e5b5989aa1e3ca20b45ab378858b2d58c5d67
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-27 06:16:53 +0600

    Add files via upload

---
+++ Sumofdigits.java
@@ -0,0 +1,16 @@
+public class Sumofdigits{
+  public static void main(String args[]){
+   java.util.Scanner abesh=new java.util.Scanner (System.in);
+   System.out.println("Enter number");
+   int n=abesh.nextInt();
+   int sum=0,temp,r;
+   temp=n;
+   while (n!=0){ //until it become 0
+     r=temp%10;   // getting the last number 
+     sum=sum+r;  // adding it
+     temp=temp/10; // dividing so that i can get the new value
+   }
+   System.out.println("Sum of digits : " +sum);
+  }}
+
+

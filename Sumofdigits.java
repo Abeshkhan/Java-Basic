ad7e3782c664d8362612bd4a2978baa6cb1a64ca
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-28 15:06:44 +0600

    Update Sumofdigits.java

--- Sumofdigits.java
+++ Sumofdigits.java
@@ -5,7 +5,7 @@ public class Sumofdigits{
    int n=abesh.nextInt();
    int sum=0,temp,r;
    temp=n;
-   while (n!=0){ //until it become 0
+   while (temp!=0){ //until it become 0
      r=temp%10;   // getting the last number 
      sum=sum+r;  // adding it
      temp=temp/10; // dividing so that i can get the new value
